68c8794b779795cd7a4cf5c092ec45f2bf99bd63
trandung2k1 <trandungitksnb2001@gmail.com>
2024-11-07 10:15:28 +0700

    init commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+app.notFound((c) => {
+  c.status(404);
+  // return c.text('404 Page Not Found', 404);
+  return c.json({
+    message: `${c.req.path} not found`,
+    code: 404,
+  });
+});
+
+app.onError((err, c) => {
+  console.error(`${err}`);
+  c.status(500);
+  return c.text('Error Message', 500);
+});
+
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
